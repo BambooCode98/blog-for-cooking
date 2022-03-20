e29e549764c8865eb4d11d68347750cfe80b628e
BambooCode98 <jw.thomas98@yahoo.com>
2022-03-19 20:47:47 -0500

    added styles

--- src/components/middle.js
+++ src/components/middle.js
@@ -1,12 +1,89 @@
 import React, { useState } from 'react';
+import '../styles/middle.css';
 
+
+function ImageSelect(props) {
+  // const [images, setImage] = useState(require("../images/cinnamoroll.png"));
+  // let idnum = props.id;
+  if(props.article.id <= 2) {
+    return(
+      <img
+      src={ require("../images/cinnamoroll.png")}
+      className='articleImage'
+      />
+    )
+    
+  } else if(props.article.id >= 3) {
+    return(
+      <img
+      src={ require("../images/riddler.png") }
+      className='articleImage'
+      />
+
+    )
+  }
+}
 
 
 export default function MiddleArticles () {
+  const [articles, setArticles] = useState([{
+    author: "Katie",
+    body: "texttexttextextextextextextextext",
+    title: "cooking",
+    id: 1,
+  }, {
+    author: "Ashlyn",
+    body: "texttexttextextextextextextextext",
+    title: "DC-Riddler",
+    id: 25,
+  }, {
+    author: "Ashlyn2",
+    body: "texttexttextextextextextextextext",
+    title: "DC-Riddler",
+    id: 3,
+  }, {
+    author: "Ashlyn4",
+    body: "texttexttextextextextextextextext",
+    title: "DC-Riddler",
+    id: 4,
+  }, {
+    author: "Katie",
+    body: "cooking time",
+    title: "Head Chef",
+    id: 2
+  }, {
+    author: "Ashlyn",
+    body: "texttexttextextextextextextextext",
+    title: "DC-Riddler",
+    id: 27,
+  }, {
+    author: "Ashlyn",
+    body: "texttexttextextextextextextextextrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
+    title: "DC-Riddler",
+    id: 29,
+  }
+  ])
+  // const [images, setImage] = useState(require("../images/cinnamoroll.png"));
+
 
 
   return(
-    <p>text</p>
+    <div>
+      {articles.map( (article) => {
+          return(
+            <div key={article.id} className="articleBlocks">
+              {/* <img
+              src={ images }
+              className='articleImage'
+              /> */}
+              < ImageSelect article={article}/>
+              <p className='articleTitle'>{article.title}</p>
+
+            </div>
+          );
+        }
+      )}
+    </div>
   )
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,20 +8,21 @@ export default function Navbar() {
     <div className="navbar">
       <div className='maintitle'>
         <h1 className='title'>Katie's Corner</h1>
+        <p className='subTitle'>A site to blog about interesting topics.</p>
       </div>
       <div className='links'>
         <ul>
           <li>
-            <a href="" className='link'>Home</a>
+            <a href="" className='link l1'>Home</a>
           </li>
           <li>
-            <a href="" className='link'>Articles</a>
+            <a href="" className='link l2'>Articles</a>
           </li>
           <li>
-            <a href="" className='link'>About</a>
+            <a href="" className='link l3'>About</a>
           </li>
           <li>
-            <a href="" className='link last'>Contact</a>
+            <a href="" className='link last l4'>Contact</a>
           </li>
         </ul>
       </div>
